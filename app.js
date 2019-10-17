540d53d4b0ade6e6c2cf90b1a83553b4d5bd4b07
Maribelle <maribelleleongzz@gmail.com>
2019-10-16 22:41:32 -0300

    initial yelpcamp to heroku

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use('/campgrounds/:id/comments', commentRoutes);
 
 ////////////
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
 	console.log('Server is running. Good');
 })
 
