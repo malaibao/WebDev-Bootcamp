b7012d732dbd607ee3332bb7a8c3566c7168ccde
Maribelle <maribelleleongzz@gmail.com>
2019-11-05 21:10:31 -0400

    improve the nav

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,9 @@ router.get('/', (req, res)=>{
 })
 
 //===========LOGIN=================//
-router.get('/login', (req, res)=>{
-	res.render('login');
-})
+router.get("/login", function(req, res){
+	res.render("login", {page: 'login'}); 
+ });
 
 router.post('/login', passport.authenticate('local', {
 	successRedirect: '/campgrounds',
@@ -22,9 +22,10 @@ router.post('/login', passport.authenticate('local', {
 })
 
 //==========register===============//
-router.get('/register', (req, res)=>{
-	res.render('register');
-})
+// show register form
+router.get("/register", function(req, res){
+	res.render("register", {page: 'register'}); 
+ });
 
 router.post('/register', (req, res)=>{
 	User.register(new User({username: req.body.username}), req.body.password, (err, user)=>{
